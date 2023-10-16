602650cfae662b13786dd694e901b71d92b7289a
Adam <adam.alrasi@outlook.com>
2023-10-17 00:03:41 +0100

    new updates

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import OpenAI from "openai";
 
 dotenv.config();
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
